536f344c589ceff1d9e4bf79a0ea4df77f9824fd
Michael Gauthier <mike@silverorange.com>
2026-05-15 20:17:29 -0300

    Add standard react configs to react config

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from 'eslint/config';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 import nodeConfig from '@silverorange/eslint-config-node';
 import { reactConfig } from './configs/react.js';
 
-export default defineConfig([nodeConfig, reactConfig]);
+export default defineConfig([
+  nodeConfig,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  reactHooks.configs.flat.recommended,
+  reactConfig,
+]);
