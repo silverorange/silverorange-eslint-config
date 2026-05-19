efd379aaf6ef5972df0f41429367370af820736c
Michael Gauthier <mike@silverorange.com>
2026-05-19 10:03:26 -0300

    Add eslint-plugin-jsx-a11y to React config

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'eslint/config';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 import nodeConfig from '@silverorange/eslint-config-node';
 import { reactConfig } from './configs/react.js';
 
@@ -9,5 +10,6 @@ export default defineConfig([
   react.configs.flat.recommended,
   react.configs.flat['jsx-runtime'],
   reactHooks.configs.flat.recommended,
+  jsxA11y.flatConfigs.recommended,
   reactConfig,
 ]);
