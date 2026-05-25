cf3d9d11194e61d247edf5d5da8ca28ad7f9f508
Michael Gauthier <mike@silverorange.com>
2026-05-25 16:35:49 -0300

    Use wider file matcher for test files that works with mjs/cjs and similar typescript files

--- packages/node/src/configs/tests.ts
+++ packages/node/src/configs/tests.ts
@@ -2,14 +2,7 @@ import type { Config } from 'eslint/config';
 
 export const testsConfig = {
   name: 'silverorange/tests',
-  files: [
-    '**/*.spec.js',
-    '**/*.spec.ts',
-    '**/*.spec.tsx',
-    '**/*.test.js',
-    '**/*.test.ts',
-    '**/*.test.tsx',
-  ],
+  files: ['**/*.spec.*', '**/*.test.*'],
   rules: {
     /** tests that use assertions on mock functions will cause false positives on this rule */
     '@typescript-eslint/unbound-method': 'off',
