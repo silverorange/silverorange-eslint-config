12b046570a6a9052ce6a4d1512bea1e3377c983f
Michael Gauthier <mike@silverorange.com>
2026-06-08 16:41:39 -0300

    Improve file matching for test files

--- packages/node/src/configs/tests.ts
+++ packages/node/src/configs/tests.ts
@@ -2,7 +2,7 @@ import type { Config } from 'eslint/config';
 
 export const testsConfig = {
   name: 'silverorange/tests',
-  files: ['**/*.spec.*', '**/*.test.*'],
+  files: ['**/*.{spec,test}.{js,ts,jsx,tsx}'],
   rules: {
     /** tests that use assertions on mock functions will cause false positives on this rule */
     '@typescript-eslint/unbound-method': 'off',
